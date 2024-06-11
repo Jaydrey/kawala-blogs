958b56d263b159a24f2b65ad1cf8ecfa470aa812
Jaydrey <jaribwetshi64@gmail.com>
2024-06-11 11:54:15 +0300

    fix(blog-type): adjusted some fields

--- types/blog.tsx
+++ types/blog.tsx
@@ -1,16 +1,14 @@
 import User from "./user";
 
 type Blog = {
-    title: string;
-    shortDescription: string;
-    publishedDate: string;
-    totalComments: number;
-    totalLikes: number;
-    author: User;
-    isUserSaved: boolean;
-    isDraft: boolean;
-}
+  title: string;
+  shortDescription: string;
+  publishedDate?: string | null;
+  totalComments: number;
+  totalLikes: number;
+  author: User;
+  isUserSaved: boolean;
+  isPublished: boolean;
+};
 
 export default Blog;
-
-
