e2fe591e8333794edb475ee98f2f5b326834cdaa
Jaydrey <jaribwetshi64@gmail.com>
2024-06-11 11:50:04 +0300

    feat(user-type): added fullName property to user type

--- types/user.tsx
+++ types/user.tsx
@@ -1,9 +1,20 @@
+type User = {
+  firstName: string;
+  middleName?: string | null;
+  lastName: string;
+  fullName: string;
+};
 
-type User  = {
-    firstName: string;
-    middleName?: string;
-    lastName: string;
-
-}
+export const createFullName = (
+  firstName: string,
+  middleName: string | null,
+  lastName: string
+): string => {
+  if (middleName) {
+    return `${firstName} ${middleName} ${lastName}`;
+  } else {
+    return `${firstName} ${lastName}`;
+  }
+};
 
 export default User;
