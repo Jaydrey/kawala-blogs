400750cd0fd8e81954e01389fa5cd00dec00c311
Jaydrey <jaribwetshi64@gmail.com>
2024-06-11 11:50:51 +0300

    refactor(home): fetch blogs and loop through each blog to get contents

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,30 @@
 import BlogCard from "@/components/BlogCard";
+import { blogs } from "@/data/blog_data";
 
 const Home = () => {
+  const allBlogs = blogs;
+
   return (
     <main className="w-full md:w-[80%] md:mx-auto flex min-h-screen flex-col gap-2">
-      <BlogCard />
-      <hr />
-      <BlogCard />
-      <hr />
+      {allBlogs &&
+        allBlogs.map((blog, blogIndex) => {
+          return (
+            <div key={blogIndex}>
+              <BlogCard
+                key={blogIndex}
+                author={blog.author}
+                title={blog.title}
+                shortDescription={blog.shortDescription}
+                totalComments={blog.totalComments}
+                totalLikes={blog.totalLikes}
+                isPublished={blog.isPublished}
+                publishedDate={blog.publishedDate ?? null}
+                isUserSaved={blog.isUserSaved}
+              />
+              <hr />
+            </div>
+          );
+        })}
     </main>
   );
 };
