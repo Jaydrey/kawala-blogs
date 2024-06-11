fa10ec5370d6ce50c71b68760347392f82a3856a
Jaydrey <jaribwetshi64@gmail.com>
2024-06-11 11:53:24 +0300

    refactor(blog-card): give dynamic fields to blog card

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -1,7 +1,7 @@
+import Blog from "@/types/blog";
 import { ChatBubbleOutlineOutlined, FavoriteBorder } from "@mui/icons-material";
 
-
-const BlogCard = () => {
+const BlogCard = (blog: Blog) => {
   return (
     <div className="w-full min-h-[20vh] flex flex-col justify-center">
       <div className="flex flex-col gap-3">
@@ -9,43 +9,50 @@ const BlogCard = () => {
           {/* image */}
           <div></div>
           {/* name of author */}
-          <div>
-            <h3>Norbert Kawala</h3>
-          </div>
+          <div>{blog.author && <h3>{blog.author.fullName}</h3>}</div>
         </div>
         <div className="flex">
           <div className="flex flex-col gap-2">
             <div>
               {/* title of blog */}
-              <h2 className="font-bold text-2xl">Postgres is eating the database world</h2>
+              {blog.title && (
+                <h2 className="font-bold text-2xl" aria-label="blog title">
+                  {blog.title}
+                </h2>
+              )}
             </div>
             <div>
-                {/* description of blog */}
-                <p className="font-normal text-base text-gray-500">PostgreSQL isn&apos;t just a simple relational database; it&apos;s a data...</p>
+              {/* description of blog */}
+              {blog.shortDescription && (
+                <p
+                  className="font-normal text-base text-gray-500"
+                  aria-label="blog description"
+                >
+                  {blog.shortDescription}
+                </p>
+              )}
             </div>
             <div className="flex justify-between text-gray-500">
-                <div className="flex gap-4">
-                    <div>
-                        {/* date published */}
-                        <time dateTime="2024-03-15">Mar 15</time>
-                    </div>
-                    <div className="flex gap-1">
-                        <FavoriteBorder />
-                        <p aria-label="total likes">2.7K</p>
-                    </div>
-                    <div className="flex gap-1">
-                        <ChatBubbleOutlineOutlined />
-                        <p aria-label="total comments">16</p>
-                    </div>
-                </div>
+              <div className="flex gap-4">
                 <div>
-
+                  {/* date published */}
+                  {blog.publishedDate && (
+                    <time dateTime="2024-03-15">{blog.publishedDate}</time>
+                  )}
                 </div>
+                <div className="flex gap-1">
+                  <FavoriteBorder />
+                  <p aria-label="total likes">{blog.totalLikes}</p>
+                </div>
+                <div className="flex gap-1">
+                  <ChatBubbleOutlineOutlined />
+                  <p aria-label="total comments">{blog.totalComments}</p>
+                </div>
+              </div>
+              <div></div>
             </div>
           </div>
-          <div>
-            {/* image of blog */}
-          </div>
+          <div>{/* image of blog */}</div>
         </div>
       </div>
     </div>
