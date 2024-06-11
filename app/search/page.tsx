cf2925e92bcd41b58b57d48f52026b7eee1cbcdb
Jaydrey <jaribwetshi64@gmail.com>
2024-06-11 12:53:39 +0300

    feat(search input field): created the search input field component

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,11 @@
+import SearchInputField from "@/components/SearchInputField";
+
 const SearchPage = () => {
-    return  <section>Search</section>
-}
+  return (
+    <main className="w-full md:w-[80%] md:mx-auto flex flex-col gap-2">
+        <SearchInputField />
+    </main>
+  );
+};
 
 export default SearchPage;
