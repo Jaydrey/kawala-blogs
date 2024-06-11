acd9a4dde8c154a7db80b5dd71145871261884e7
Jaydrey <jaribwetshi64@gmail.com>
2024-06-11 10:45:47 +0300

    feat(home): added the blog cards for each blog published

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import BlogCard from "@/components/BlogCard";
+
 const Home = () => {
-  return <main className="w-full flex min-h-screen flex-col">
-    Hello there this is jarib
-  </main>;
-}
+  return (
+    <main className="w-full md:w-[80%] md:mx-auto flex min-h-screen flex-col gap-2">
+      <BlogCard />
+      <hr />
+      <BlogCard />
+      <hr />
+    </main>
+  );
+};
 
 export default Home;
