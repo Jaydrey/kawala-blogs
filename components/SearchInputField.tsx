c64c8525f4dce5cb46edd545a20a73c143d1bacb
Jaydrey <jaribwetshi64@gmail.com>
2024-06-11 15:13:49 +0300

    feat(search input field): push state to the url

--- components/SearchInputField.tsx
+++ components/SearchInputField.tsx
@@ -1,6 +1,17 @@
+"use client";
 import { Search } from "@mui/icons-material";
+import { useRouter } from "next/navigation";
 
 const SearchInputField = () => {
+  const router = useRouter();
+
+  const handleSearchTermChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    router.replace(`?term=${event.target.value}`, {
+      scroll: false,
+    });
+  };
   return (
     <div className="w-full flex">
       <div className="py-2 px-3 flex items-center border-l border-y rounded-l-md">
@@ -9,9 +20,13 @@ const SearchInputField = () => {
       <input
         className="w-full py-2 px-3 text-lg placeholder:text-sm placeholder:font-medium border-y border-r border-gray-200 rounded-r-md focus:outline-none"
         type="text"
+        name="searchBlogs"
+        id="searchBlogs"
+        autoFocus
         aria-label="search blogs field"
         role="search"
         placeholder="Search all blogs ..."
+        onChange={handleSearchTermChange}
       />
     </div>
   );
