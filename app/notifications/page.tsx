d3e4a08f7a174189f6dcfd75ff4c6b8311396c5f
Jaydrey <jaribwetshi64@gmail.com>
2024-06-11 17:23:20 +0300

    feat(notification): fecth and display notifications

--- app/notifications/page.tsx
+++ app/notifications/page.tsx
@@ -1,7 +1,26 @@
+import NotificationMessage from "@/components/NotificationMessage";
+import UserNotificationCard from "@/components/UserNotificationCard";
+import notifications from "@/data/notification_data";
+
 const NotificationPage = () => {
-    return <main className="w-full md:w-[80%] md:mx-auto flex flex-col gap-2">
-        
+  const allNotifications = notifications;
+  return (
+    <main className="w-full md:w-[80%] md:mx-auto flex flex-col gap-2">
+      {allNotifications &&
+        allNotifications.map((notification, notificationIndex) => {
+          return notification.userMessage ? (
+            <UserNotificationCard
+              key={notificationIndex}
+              notification={notification}
+            />
+          ) : notification.message ? (
+            <NotificationMessage notification={notification} />
+          ) : (
+            <></>
+          );
+        })}
     </main>
-}
+  );
+};
 
 export default NotificationPage;
