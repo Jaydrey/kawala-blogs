8bac4563921f61844dc9b40ad2811739d62a79df
Jaydrey <jaribwetshi64@gmail.com>
2024-07-04 19:45:05 +0300

    feat(header): show active when nav bar item is clicked

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,16 @@
+"use client";
 import Image from "next/image";
 import { Search, Notifications, NoteAlt } from "@mui/icons-material";
 import Link from "next/link";
+import SearchForm from "./SearchForm";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const paths = usePathname();
+  const isActive = (path: string) => paths === path;
   return (
     <header className="w-full min-h-[90px] flex justify-between items-center sticky">
-      <div>
+      <div className="flex items-center gap-4">
         <Link href="/">
           <div>
             <Image
@@ -16,22 +21,37 @@ const Header = () => {
             />
           </div>
         </Link>
+        <div className="hidden md:block">
+          <SearchForm />
+        </div>
       </div>
       <nav className="flex">
         <ul className="flex gap-6">
-          <li>
+          <li className="md:hidden" key="search">
             <Link href="/search" aria-label="search blogs">
-              <Search className="text-gray-500" />
+              <Search
+                className={`text-gray-500 ${
+                  isActive("/search") ? "text-gray-800" : "text-gray-500"
+                }`}
+              />
             </Link>
           </li>
-          <li>
+          <li key="notification">
             <Link href="/notifications" aria-label="notification">
-              <Notifications className="text-gray-500" />
+              <Notifications
+                className={`text-gray-500 ${
+                  isActive("/notifications") ? "text-gray-800" : "text-gray-500"
+                }`}
+              />
             </Link>
           </li>
-          <li>
+          <li key="write">
             <Link href="/write" aria-label="write blog">
-              <NoteAlt className="text-gray-500" />
+              <NoteAlt
+                className={`text-gray-500 ${
+                  isActive("/write") ? "text-gray-800" : "text-gray-500"
+                }`}
+              />
             </Link>
           </li>
         </ul>
